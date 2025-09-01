b6e7a5844811bd9aa3cefe573f4ef0f61b42e4b8
Ryan Addessi <raddessi@users.noreply.github.com>
2025-08-31 20:39:48 -0600

    feat: crappy test function

--- src/saltext/dockermod/modules/dockermod.py
+++ src/saltext/dockermod/modules/dockermod.py
@@ -338,6 +338,9 @@ def __virtual__():
     return (False, "Could not import docker module, is docker-py installed?")
 
 
+def zzz():
+    return "custom"
+
 def _file_client():
     """
     Return a file client
